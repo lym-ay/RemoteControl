2f7e790202d06052d3b2a1fc531f6f63649db90e
lym-ay <lym_ay@sina.com>
2017-07-03 10:38:35 +0800

    add new file
    add new file

---
+++ Helper/CommonHeadFile.m
@@ -0,0 +1,13 @@
+//
+//  CommonHeadFile.m
+//  RemoteControl
+//
+//  Created by olami on 2017/7/3.
+//  Copyright © 2017年 VIA Technologies, Inc. & OLAMI Team. All rights reserved.
+//
+
+#import "CommonHeadFile.h"
+
+@implementation CommonHeadFile
+
+@end
--- Helper/InfraredData.h
+++ Helper/InfraredData.h
@@ -9,5 +9,8 @@
 #import <Foundation/Foundation.h>
 
 @interface InfraredData : NSObject
-
+@property (nonatomic, copy) NSString *buttonID;//按钮对应的ID
+@property (nonatomic, copy) NSString *buttonEnglishName;//按钮对应功能的英文名称
+@property (nonatomic, copy) NSString *buttonChineseName;//按钮对应功能的中文名称
+@property (nonatomic, copy) NSArray  *buttonPlusData;//按钮对应的红外线码
 @end
